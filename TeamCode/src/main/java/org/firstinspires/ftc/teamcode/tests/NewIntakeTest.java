1dfc524e21587f88558d39317290ec124b8d4768
aadityasah <aaditya.sah365@gmail.com>
2026-04-17 15:07:04 -0400

    new intake test change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/NewIntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/NewIntakeTest.java
@@ -3,32 +3,42 @@ package org.firstinspires.ftc.teamcode.tests;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.common.Intake;
+import org.firstinspires.ftc.teamcode.common.Shooter;
 
 @TeleOp(name = "NewIntakeTest")
 public class NewIntakeTest extends LinearOpMode {
 
     public Intake intake = null;
-
+    public Shooter shooter = null;
     public CRServo intakeServo = null;
 
     @Override
-    public void runOpMode(){
+    public void runOpMode() {
 
         intake = new Intake(hardwareMap);
-
+        shooter = new Shooter(hardwareMap);
         intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
 
         waitForStart();
 
-        while(opModeIsActive() && !isStopRequested()){
+        while (opModeIsActive() && !isStopRequested()) {
             intake.lowerSwingArm();
-            if(gamepad1.right_trigger>0.5){
+
+            if(gamepad1.dpad_right){
+                shooter.setShooterVelocity(1500);
+            } else if (gamepad1.dpad_left) {
+                shooter.setShooterVelocity(0);
+            }
+
+            if (gamepad1.x) {
+                intake.setIntakeState(Intake.IntakeState.ON);
+                intakeServo.setPower(-1.0);
+            } else if (gamepad1.b) {
                 intake.setIntakeState(Intake.IntakeState.ON);
                 intakeServo.setPower(1.0);
-            } else {
+            } else if (gamepad1.a) {
                 intake.setIntakeState(Intake.IntakeState.OFF);
                 intakeServo.setPower(0.0);
             }
