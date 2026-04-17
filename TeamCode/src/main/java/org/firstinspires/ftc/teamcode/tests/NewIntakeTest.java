61ca42fd0f5c9e16f44574c48e8c7ca844295c39
aadityasah <aaditya.sah365@gmail.com>
2026-04-17 12:37:31 -0400

    new intake test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/NewIntakeTest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.common.Intake;
+
+@TeleOp(name = "NewIntakeTest")
+public class NewIntakeTest extends LinearOpMode {
+
+    public Intake intake = null;
+
+    public CRServo intakeServo = null;
+
+    @Override
+    public void runOpMode(){
+
+        intake = new Intake(hardwareMap);
+
+        intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
+
+        waitForStart();
+
+        while(opModeIsActive() && !isStopRequested()){
+            if(gamepad1.right_trigger>0.5){
+                intake.setIntakeState(Intake.IntakeState.ON);
+                intakeServo.setPower(1.0);
+            } else {
+                intake.setIntakeState(Intake.IntakeState.OFF);
+                intakeServo.setPower(0.0);
+            }
+        }
+    }
+}
